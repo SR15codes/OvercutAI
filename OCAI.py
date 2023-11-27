8c8be993391eca05873860d0b13f3f2e767ecd03
SR15codes <137059826+SR15codes@users.noreply.github.com>
2023-11-27 19:08:37 +0530

    Update OCAI.py

--- OCAI.py
+++ OCAI.py
@@ -10,30 +10,30 @@ def main():
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image', use_column_width=True)
 
-    a='model.h5'
-    from keras.models import load_model
-    x = load_model(a)
-
-    test_photo = uploaded_file
-    img = cv2.imread(test_photo)
-
-    size=(250,250)
-    background_color="white"
-    resized = cv2.resize(img, size, interpolation = cv2.INTER_AREA)
-    print('Resized Dimensions : ',resized.shape)
-
-    print(resized.shape)
-    img2 = (np.expand_dims(resized,0))
-
-    print(img2.shape)
-    predictions_single = x.predict(img2)
-
-    print(predictions_single)
-    if (predictions_single[0][0]>predictions_single[0][1]):
-        print('Predicted class is Accident')
-     else:
-        print('Predicted class is Non Accident')
-            #trained model and prediction ends
+        a='model.h5'
+        from keras.models import load_model
+        x = load_model(a)
+    
+        test_photo = uploaded_file
+        img = cv2.imread(test_photo)
+    
+        size=(250,250)
+        background_color="white"
+        resized = cv2.resize(img, size, interpolation = cv2.INTER_AREA)
+        print('Resized Dimensions : ',resized.shape)
+    
+        print(resized.shape)
+        img2 = (np.expand_dims(resized,0))
+    
+        print(img2.shape)
+        predictions_single = x.predict(img2)
+    
+        print(predictions_single)
+        if (predictions_single[0][0]>predictions_single[0][1]):
+            print('Predicted class is Accident')
+        else:
+            print('Predicted class is Non Accident')
+                #trained model and prediction ends
     else:
         st.info('Please upload an image.')
 
